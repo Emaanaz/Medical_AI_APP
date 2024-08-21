5671d43dd5e1e38d7a713825f84a84aea2f5ef8e
Aman Sande <107488324+Emaanaz@users.noreply.github.com>
2024-08-21 22:27:10 +0530

    app.py

--- app.py
+++ app.py
@@ -21,31 +21,7 @@ st.set_page_config(
   #  layout="",  # Page layout option
 )
 
-generation_config = {
-    "temperature": 1,
-    "top_p": 0.95,
-    "top_k": 0,
-    "max_output_tokens": 8192,
-}
 
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-]
 
 system_prompts = [
     """
@@ -83,9 +59,7 @@ system_prompts = [
 """
 ]
 
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
+
 
 
 
